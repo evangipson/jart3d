912d1657e5d0fabb97c625f234cd8b531840abf0
Evan Gipson <evangipson@gmail.com>
2019-10-01 20:51:43 -0500

    Initial commit of Unity files with working jart
    IT'S ALIVEEEE

---
+++ Assets/SetCameraPosition.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetCameraPosition : MonoBehaviour
+{
+	private void centerCameraOnJartboard()
+	{
+		Camera camera = GetComponent<Camera>();
+		camera.transform.position = new Vector3(0.5f, 0.5f, -1);
+	}
+
+	// Start is called before the first frame update
+	void Start()
+	{
+		centerCameraOnJartboard();
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/jart.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class jart : MonoBehaviour
+{
+	public int jartletAmount = 10;
+	// I want to use System Random, not UnityEngine Random
+	private System.Random randomizer = new System.Random();
+	private Sprite jartboard;
+	private Color[] possibleColors = {
+		Color.red,
+		Color.blue,
+		Color.green,
+		Color.cyan,
+		Color.magenta,
+		Color.yellow
+	};
+	private List<Sprite> jartlets = new List<Sprite>();
+	private Sprite sprite;
+	private int jartboardHeight = 10;
+	private int jartboardWidth = 10;
+
+	/**
+	 * Creates a sprite. Intended to be used to create Jartlets
+	 * and the Jartboard, which is returned by default (using
+	 * default parameters). */
+	private Sprite createSprite(Color color, int width = 1, int height = 1, float originX = 0, float originY = 0)
+	{
+		GameObject go = new GameObject();
+		SpriteRenderer sr = go.AddComponent<SpriteRenderer>();
+		sprite = Sprite.Create(new Texture2D(width, height), new Rect(0, 0, width, height), new Vector2(0, 0), 100.0f);
+		
+		// set a color
+		sr.material.color = color;
+		sr.sprite = sprite;
+
+		// set the position of the object
+		go.transform.position = new Vector3(originX / 10, originY / 10, 0);
+		go.transform.localScale = new Vector3(width - originX, height - originY, 0);
+
+		return sprite;
+	}
+
+	private void createJartlets()
+	{
+		for (int i = 0; i < jartletAmount; i++)
+		{
+			jartlets.Add(createSprite(
+				possibleColors[randomizer.Next(0, possibleColors.Length - 1)],
+				randomizer.Next(1, jartboardWidth),
+				randomizer.Next(1, jartboardHeight),
+				randomizer.Next(0, jartboardWidth),
+				randomizer.Next(0, jartboardHeight)
+			));
+		}
+	}
+
+	private void createJartboard()
+	{
+		jartboard = createSprite(Color.white, jartboardWidth, jartboardHeight);
+	}
+
+	public void Start()
+	{
+		createJartboard();
+		createJartlets();
+		// render out newly created jartlets and jartboard
+	}
+}
